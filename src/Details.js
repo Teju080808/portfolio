a78e16f6ab422e05e57e92df6ca964659d75a7f5
Your Name <your_email@example.com>
2025-10-01 16:37:20 +0530

    Initial commit

---
+++ src/Details.js
@@ -0,0 +1,68 @@
+// Enter all your detials in this file
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Tejal Purabiya",
+  tagline: "I build things for web",
+  img: profile,
+  about: `my name is Tejal Purabiya. I have completed my BCA from Monark University. Currently, I am learning Full Stack Development at Red & White Multimedia Education. I have finished the front-end part and I am now learning the back-end.
+I am a quick learner and I enjoy coding. I am looking for a job where I can use my skills, learn more, and grow as a developer.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor Of Computer Application",
+    Company: `Monark University`,
+    Location: "Ahmedabad"
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  github: github,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Moliva Travel",
+    image: projectImage1,
+    description: `A travel website built using HTML, CSS, JavaScript. It shows destination info, packages, contact etc.`,
+    techstack: "HTML, CSS, JavaScript",
+    previewLink: "https://teju080808.github.io/Moliva-Travel/", // agar deploy kiya hai
+    githubLink: "https://github.com/Teju080808/Moliva-Travel",
+  }
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "tejalpurabiya08@gmail.com",
+  phone: "+91 97273 86528",
+};
